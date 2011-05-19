0dcfd8d1a7d71bfc4e68b5dc0bbbabdf7d490bb4
Eugene Ma <edma2@Section9.localdomain>
2011-05-18 18:59:41 -0700

    working model

--- stegan.py
+++ stegan.py
@@ -1,12 +1,11 @@
 # stegan.py - TODO: more channels
-import sys, os
-import Image
-import subprocess, tempfile
+import sys, os, struct
+import Image, subprocess
 
 # Check usage and flags
-if len(sys.argv) != 5:
-        print "usage: stegan <mode> <reference image> <input file> <output file>"
-        print "usage: available modes: encode, decode"
+if len(sys.argv) != 4:
+        print "Usage: stegan <mode> <reference image> <input file>"
+        print "Usage: available modes: encode, decode"
         sys.exit()
 mode = sys.argv[1]
 
@@ -16,51 +15,40 @@ refpix = refim.load()
 refw, refh = refim.size
 
 if mode == "encode":
-        f = open(sys.argv[3])
-        flen = os.path.getsize(sys.argv[3])
-        compress_tmp_file = tempfile.NamedTemporaryFile()
-
         # Sanity check
-        if (refw * refh) < (flen * 8):
-                print >> sys.stderr, "error: input file too large for reference image"
+        if (refw * refh) < (os.path.getsize(sys.argv[3]) * 8):
+                print "Error: input file too large for reference image"
                 sys.exit()
 
-        # Compress
-        print >> sys.stderr, "Compressing data...",
-        compress_args = ("gzip -c %s" % sys.argv[3]).split()
-        p = subprocess.Popen(compress_args, stdout = compress_tmp_file, stderr = subprocess.PIPE) 
+        # Compress to .gz file (input => input.gz)
+        print "Compressing data...",
+        gzargs = ("gzip -f %s" % sys.argv[3]).split()
+        gzname = "%s.gz" % sys.argv[3]
+        p = subprocess.Popen(gzargs)
         p.wait()
-        size = os.path.getsize(compress_tmp_file.name)
-        print >> sys.stderr, "Done. (Compressed size: %d bytes)" % size
+        print "Done. (Compressed size: %d bytes)" % os.path.getsize(gzname)
 
-        # Encrypt
-        encrypt_args = ("filelock %s" % compress_tmp_file.name).split()
-        print >> sys.stderr, "Encrypting data..."
-        p = subprocess.Popen(encrypt_args, stdout = subprocess.PIPE) 
-        data = p.communicate()[0]
-        size = len(data)
-        print >> sys.stderr, "Done. (Encrypted size: %d bytes)" % size
+        # Encrypt compressed data and add .enc suffix (input.gz => input.gz.enc)
+        print "Encrypting data..."
+        encargs = ("filelock %s" % gzname).split()
+        encname = "%s.enc" % gzname
+        encf = open(encname, "w")
+        p = subprocess.Popen(encargs, stdout = encf) 
+        p.wait()
+        encf.close()
+        print "Done. (Encrypted size: %d bytes)" % os.path.getsize(encname)
 
-        # Encode length of data into image
-        binsize = bin(size)[2:].zfill(32)
-        bitsencoded = 0
-        print >> sys.stderr, "Encoding length...",
+        # Assume little endian byte order when packing size
+        packedsize = struct.pack('i', os.path.getsize(encname))
+
+        # Encode encrypted data into image pixels (input.gz.enc => input.gz.enc.png)
+        print "Encoding data...",
+        f = open(encname, "r")
         (x, y) = (0, 0)
-        for bit in binsize:
-                pixel = list(refpix[x, y])
-                if pixel[0] == 0:
-                        pixel[0] += int(bit)
-                else:
-                        pixel[0] -= int(bit)
-                refpix[x, y] = tuple(pixel)
-                (x, y) = (x+1, y) if x+1 < refw else (0, y+1)
-                bitsencoded += 1
-        print >> sys.stderr, "Done. (Length: %d bytes)" % size
-
-        # Encode file
-        print >> sys.stderr, "Encoding file...",
-        bytesread = 0
-        for byte in data:
+        for i in range(os.path.getsize(encname)+4):
+                # Encode length in little-endian order
+                byte = packedsize[i] if i < 4 else f.read(1)
+                # Convert from base 16 to binary
                 byte = (bin(ord(byte))[2:]).zfill(8)
                 for bit in byte:
                         pixel = list(refpix[x, y])
@@ -70,72 +58,71 @@ if mode == "encode":
                                 pixel[0] -= int(bit)
                         refpix[x, y] = tuple(pixel)
                         (x, y) = (x+1, y) if x+1 < refw else (0, y+1)
-                        bitsencoded += 1
-                bytesread += 1
-        compress_tmp_file.close()
-        refim.save(sys.argv[4], "PNG")
-        print >> sys.stderr, "Done. (%d bytes read, %d bits encoded)" % (bytesread, bitsencoded)
+        f.close()
+        print "Done."
+
+        # Clean up .gz and .enc files
+        print "Cleaning up...",
+        os.remove(gzname)
+        os.remove(encname)
+        print "Done"
+
+        # Save output with .png extension
+        print "Saving to %s.png." % encname
+        refim.save("%s.png" % encname, "PNG")
+
 elif mode == "decode":
         # Open input image
         inim = Image.open(sys.argv[3]).convert("RGB")
         inpix = inim.load()
         inw, inh = inim.size
 
-        bitsdecoded = 0
-        byteswritten = 0
-
-        decode_tmp_file = tempfile.NamedTemporaryFile()
-        decrypt_tmp_file = tempfile.NamedTemporaryFile()
-        decompress_output_file = open(sys.argv[4], "w")
-
-        # Decode length
-        print >> sys.stderr, "Decoding length...",
-        size = []
+        # Decode image (input.gz.enc.png => input.gz.enc)
+        print "Decoding..."
+        f = open("%s" % sys.argv[3][:-4], "w")
         (x, y) = (0, 0)
-        for i in range(32):
-                if inpix[x, y][0] != refpix[x, y][0]:
-                        size.append('1')
-                else:
-                        size.append('0')
-                (x, y) = (x+1, y) if x+1 < inw else (0, y+1)
-                bitsdecoded += 1
-        size = int("".join(size), 2)
-        print >> sys.stderr, "Done. (Length: %d bytes)" % size
-
-        # Decode image
-        print >> sys.stderr, "Decoding input image...",
-        for i in range(size):
-                byte = []
+        i, size, length = 0, 4, 0
+        # Update size dynamically 
+        while i < size:
+                # Decode byte and convert to an integer
+                byte = 0
                 for j in range(8):
-                        if inpix[x, y][0] != refpix[x, y][0]:
-                                byte.append('1')
-                        else:
-                                byte.append('0')
+                        if inpix[x, y][0] != refpix[x, y][0]: byte |= (1<<(7-j))
                         (x, y) = (x+1, y) if x+1 < inw else (0, y+1)
-                        bitsdecoded += 1
-                byte = int("".join(byte), 2)
-                decode_tmp_file.write(chr(byte))
-                byteswritten += 1
-        print >> sys.stderr, "Done. (%d bits decoded, %d bytes written)" % (bitsdecoded, byteswritten)
+                # Decode length before data (little endian)
+                if i < 4:
+                        length |= (byte << (i*8))
+                        if i == 3: 
+                                size += length
+                                print "Length: %d bytes" % length
+                else:
+                        f.write(chr(byte))
+                i += 1
 
-        # Decrypt
-        print >> sys.stderr, "Decrypting file..."
-        decrypt_args = ("filelock -d %s" % decode_tmp_file.name).split()
-        p = subprocess.Popen(decrypt_args, stdout = decrypt_tmp_file) 
+        # Close and flush write buffer
+        f.close()
+        print "Done. (%d bytes written)" % os.path.getsize("%s" % sys.argv[3][:-4])
+
+        # Decrypted decoded file (input.gz.enc => input.gz)
+        print "Decrypting data..."
+        decrargs = ("filelock -d %s" % sys.argv[3][:-4]).split()
+        decrf = open("%s" % sys.argv[3][:-8], "w")
+        p = subprocess.Popen(decrargs, stdout = decrf) 
         p.wait()
-        size = os.path.getsize(decrypt_tmp_file.name)
-        print >> sys.stderr, "Done. (%d bytes decrypted)" % size
+        decrf.close()
+        print "Done. (%d bytes decrypted)" % os.path.getsize("%s" % sys.argv[3][:-8])
 
-        print >> sys.stderr, "Decompressing file...",
-        decompress_args = ("gzip -c -d %s" % decrypt_tmp_file.name).split()
-        p = subprocess.Popen(decompress_args, stdout = decompress_output_file)
+        # Decompress (input.gz => input)
+        print "Decompressing data...",
+        gzargs = ("gzip -d %s" % sys.argv[3][:-8]).split()
+        p = subprocess.Popen(gzargs)
         p.wait()
-        print >> sys.stderr, "Done. (%d bytes decompressed)" % os.path.getsize(sys.argv[4])
+        print "Done. (%d bytes decompressed)" % os.path.getsize("%s" % sys.argv[3][:-11])
 
-        # Close and delete temporary files
-        decode_tmp_file.close()
-        decrypt_tmp_file.close()
-        decompress_output_file.close()
+        # Clean up
+        os.remove(sys.argv[3])
+        os.remove(sys.argv[3][:-4])
+
 else:
-        print >> sys.stderr, "error: unknown mode"
-        print >> "usage: available modes: encode, decode"
+        print "Error: unknown mode"
+        print "Usage: available modes: encode, decode"
