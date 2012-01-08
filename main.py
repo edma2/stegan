5a7bce63e11d547022d3bf609bcd93964f1f6f87
Eugene Ma <edma2@san.rr.com>
2012-01-07 16:32:42 -0800

    Fleshed out main.py.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import stegan, Image, getpass
+import stegan, sys, Image, getpass
 
 pw = getpass.getpass()
 
-#image = Image.open('tux.png')
-#inp = open('README')
-#data = inp.read()
-#stegan.encode(image, pw, data)
-#image.save('out.png')
-
-image = Image.open('out.png')
-print stegan.decode(image, pw)
+if sys.argv[1] == 'encode':
+    image = Image.open(sys.argv[2])
+    inp = open(sys.argv[3])
+    data = inp.read()
+    stegan.encode(image, pw, data)
+    image.save(sys.argv[4])
+elif sys.argv[1] == 'decode':
+    image = Image.open(sys.argv[2])
+    data = stegan.decode(image, pw)
+    out = open(sys.argv[3], 'w')
+    out.write(data)
