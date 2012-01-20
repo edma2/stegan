df6ddd031628da715e16c07192ce70a6ca408c2c
Eugene Ma <edma2@san.rr.com>
2012-01-20 14:38:23 -0800

    Cleanup.

--- src/stegan.py
+++ src/stegan.py
@@ -1,37 +1,33 @@
-import Image, random
+import random, struct, hashlib, gzip, StringIO
+import Image, Crypto.Cipher.Blowfish as blowfish
 
-class Steganographer:
+class Steganographer(object):
     """Provides an interface for reading and writing raw bytes into image
-    pixels. @image is the image to be used as the container, a PIL Image
-    object, using the RGB color model."""
+    pixels."""
 
-    def __init__(self, im):
+    def __init__(self, im, iter):
         self.im = im
         self.pixels = self.im.load()
         w, h = self.im.size
-        self.iter = ((x,y,c) for x in range(w) for y in range(h) for c in range(3))
+        self.iter = iter
 
     def write_bit(self, bit):
-        """Embeds bit in image by settings the LSB of a channel on or off."""
         (x, y, ch) = self.iter.next()
         pixel = list(self.pixels[x, y])
         pixel[ch] = (pixel[ch] | 1) if bit else (pixel[ch] & ~1)
         self.pixels[x, y] = tuple(pixel)
 
     def read_bit(self):
-        """Reads the next encoded bit."""
         (x, y, ch) = self.iter.next()
         pixel = self.pixels[x, y]
         bit = pixel[ch] & 1
         return bit
 
     def write_byte(self, byte):
-        """Embeds byte into image starting with the LSB."""
         for i in range(8):
             self.write_bit((byte & (1<<i)) >> i)
 
     def read_byte(self):
-        """Reads the next 8 encoded bits and return as byte."""
         byte = 0
         for i in range(8):
             bit = self.read_bit()
@@ -39,12 +35,10 @@ class Steganographer:
         return byte
 
     def write(self, str):
-        """Embeds byte string into image."""
         for byte in str:
             self.write_byte(ord(byte))
 
     def read(self, length):
-        """Reads the next length bytes and return as string."""
         str = ''
         for i in range(length):
             str += chr(self.read_byte())
@@ -55,3 +49,79 @@ class Steganographer:
         random.seed(seed)
         random.shuffle(available)
         self.iter = (pos for pos in available)
+
+class Writer(Steganographer):
+    """Writes a single string of bytes to image, including a header used for
+    decoding"""
+
+    def __init__(self, im, passphrase):
+        self.key = hashlib.sha256(passphrase).digest()[:7]
+        self.iv = struct.pack("Q", random.getrandbits(64))
+        w, h = im.size
+        linear = ((x,y,c) for x in range(w) for y in range(h) for c in range(3))
+        super(Writer, self).__init__(im, linear)
+
+    def compress(self, str):
+        """Returns the string in compressed form"""
+        buf = StringIO.StringIO()
+        # Write compressed string to string buffer
+        gz = gzip.GzipFile(mode = 'w', fileobj = buf)
+        gz.write(str)
+        gz.close()
+        # Get string from buffer
+        return buf.getvalue()
+
+    def encrypt(self, plaintext):
+        """Encrypts plaintext and returns ciphertext."""
+        def bf_padding(data):
+            """Returns padding necessary for Blowfish algorithm."""
+            if len(data) == 8:
+                return ''
+            else:
+                return '\x00' * (8 - len(data) % 8)
+        cipher = blowfish.new(self.key, blowfish.MODE_OFB, self.iv)
+        ciphertext = cipher.encrypt(plaintext + bf_padding(plaintext))
+        return ciphertext
+
+    def encode(self, str):
+        plaintext = self.compress(str)
+        ciphertext = self.encrypt(plaintext)
+        # Write header
+        self.write(struct.pack('i', len(ciphertext)) + self.iv)
+        # Scramble iterator and write payload
+        self.randomize(self.iv)
+        self.write(ciphertext)
+
+class Reader(Steganographer):
+
+    def __init__(self, im, passphrase):
+        self.key = hashlib.sha256(passphrase).digest()[:7]
+        w, h = im.size
+        linear = ((x,y,c) for x in range(w) for y in range(h) for c in range(3))
+        super(Reader, self).__init__(im, linear)
+
+    def decompress(self, str):
+        """Returns the (GZip compressed) string in decompressed form"""
+        buf = StringIO.StringIO(str)
+        # Read compressed data into gzip object, decompress
+        gz = gzip.GzipFile(mode = 'r', fileobj = buf)
+        # Get string
+        output = gz.read()
+        # Cleanup
+        gz.close()
+        buf.close()
+        return output
+
+    def decrypt(self, ciphertext):
+        """Decrypts ciphertext, returning plaintext"""
+        cipher = blowfish.new(self.key, blowfish.MODE_OFB, self.iv)
+        plaintext = cipher.decrypt(ciphertext)
+        return plaintext
+
+    def decode(self):
+        length = self.read(4)
+        length = struct.unpack('i', length)[0]
+        self.iv = self.read(8)
+        self.randomize(self.iv)
+        ciphertext = self.read(length)
+        return self.decompress(self.decrypt(ciphertext))
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import sys, Image, getpass
 sys.path.append('src')
-import encode, decode
+import stegan
 
-image = Image.open('test/tux.png').convert('RGB')
-encode.encode(image, 'password', 'hello, world!')
-image.save("test/test.png")
-image = Image.open('test/test.png').convert('RGB')
-print decode.decode(image, 'password')
+im = Image.open('test/tux.png').convert('RGB')
+writer = stegan.Writer(im, 'password')
+writer.encode('hello, world')
+im.save("test/test.png")
+
+im = Image.open('test/test.png').convert('RGB')
+reader = stegan.Reader(im, 'password')
+print reader.decode()
